a01f367183c963de877e7ff4ae5919d2e48545d3
unknown <artm-marych@rambler.ru>
2020-07-21 20:50:57 +0200

    addShopping

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,10 +1,15 @@
 class Header
 {
+  handlerOpenShoppingPage()
+  {
+    shoppingPage.render();
+  }
+
   render(count)
   {
     const html = `
     <div class="header-container">
-    <div class="header-counter">${count}</div>
+      <div class="header-counter" onclick="headerPage.handlerOpenShoppingPage();">📜${count}</div>
     </div>
     `;
     ROOT_HEADER.innerHTML = html;
